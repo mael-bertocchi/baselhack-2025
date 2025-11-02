572a934690ebc0204dd6dc80b9f1dd2ed3e58c5e
MaxEntz <maxime.entz@epitech.eu>
2025-11-02 01:40:07 +0100

    feat: add a new route for stat, to see the sorted list of open's topics.

--- code/backend/src/modules/stats/stats.types.ts
+++ code/backend/src/modules/stats/stats.types.ts
@@ -1,1 +1,9 @@
-
+/**
+ * @interface TopicWithSubmissionCount
+ * @description Represents a topic with its submission count
+ */
+export interface TopicWithSubmissionCount {
+    topicId: string;
+    title: string;
+    submissionCount: number;
+}
